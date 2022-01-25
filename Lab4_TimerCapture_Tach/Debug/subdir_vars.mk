27d027b99f0c984fcabaf582aadfb2ff0f8f0f57
elaynetron <56335018+elaynetron@users.noreply.github.com>
2022-01-26 03:53:49 +0800

    Add files via upload

---
+++ Lab4_TimerCapture_Tach/Debug/subdir_vars.mk
@@ -0,0 +1,107 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/Bump.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/Clock.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/CortexM.c \
+../Lab4_Tachmain.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/LaunchPad.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/Motor.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/PWM.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/SysTick.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/TA3InputCapture.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/TExaS.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/TimerA1.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Bump.d \
+./Clock.d \
+./CortexM.d \
+./Lab4_Tachmain.d \
+./LaunchPad.d \
+./Motor.d \
+./PWM.d \
+./SysTick.d \
+./TA3InputCapture.d \
+./TExaS.d \
+./TimerA1.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Bump.obj \
+./Clock.obj \
+./CortexM.obj \
+./Lab4_Tachmain.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./PWM.obj \
+./SysTick.obj \
+./TA3InputCapture.obj \
+./TExaS.obj \
+./TimerA1.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Bump.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"Lab4_Tachmain.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"PWM.obj" \
+"SysTick.obj" \
+"TA3InputCapture.obj" \
+"TExaS.obj" \
+"TimerA1.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Bump.d" \
+"Clock.d" \
+"CortexM.d" \
+"Lab4_Tachmain.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"PWM.d" \
+"SysTick.d" \
+"TA3InputCapture.d" \
+"TExaS.d" \
+"TimerA1.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/Bump.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/Clock.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/CortexM.c" \
+"../Lab4_Tachmain.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/LaunchPad.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/Motor.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/PWM.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/SysTick.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/TA3InputCapture.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/TExaS.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/TimerA1.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
---
+++ Lab4ref_ADCSWTrigger/Debug/subdir_vars.mk
@@ -0,0 +1,77 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/ADC14.c \
+../ADCSWTriggermain.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/Clock.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/CortexM.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/LaunchPad.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/SysTickInts.c \
+C:/Users/etan074/Desktop/CE2007-Students\ Elayne/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./ADC14.d \
+./ADCSWTriggermain.d \
+./Clock.d \
+./CortexM.d \
+./LaunchPad.d \
+./SysTickInts.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./ADC14.obj \
+./ADCSWTriggermain.obj \
+./Clock.obj \
+./CortexM.obj \
+./LaunchPad.obj \
+./SysTickInts.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"ADC14.obj" \
+"ADCSWTriggermain.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"LaunchPad.obj" \
+"SysTickInts.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"ADC14.d" \
+"ADCSWTriggermain.d" \
+"Clock.d" \
+"CortexM.d" \
+"LaunchPad.d" \
+"SysTickInts.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/ADC14.c" \
+"../ADCSWTriggermain.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/Clock.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/CortexM.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/LaunchPad.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/SysTickInts.c" \
+"C:/Users/etan074/Desktop/CE2007-Students Elayne/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
